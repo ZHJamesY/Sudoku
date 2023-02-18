b5db1bcf94887673c0dce6e2db428e94dd929d29
James <1195771246y@gmail.com>
2023-02-17 20:29:16 -0500

    added UML

--- Code/Scripts/Sudoku.js
+++ Code/Scripts/Sudoku.js
@@ -1,6 +1,7 @@
 // Board size variable
 const SIZE = 9;
 
+// 0's represent empty cell
 let board = [
     "010000010",
     "001000100",
@@ -13,6 +14,7 @@ let board = [
     "001000000",
 ];
 
+// solution
 let Sol = [
     "687149253",
     "514362987",
@@ -31,6 +33,7 @@ $(function(){
     let p_board = document.getElementById("Palette_Board");
     let p_row = p_board.insertRow(0);
 
+    // Generate col thick borders
     for(let t = 0; t < 3; t++)
     {
         let colGroup = document.createElement("colgroup");
@@ -48,15 +51,16 @@ $(function(){
         p_cell.id = "p" + (i + 1).toString();
         let g_row = g_board.insertRow(i);
 
+        // for every third row add thick top border
         if(i % 3 == 0 && i > 0)
         {
-            // for every third row add thick top border
             g_row.classList.add("rowBorder");
         }
 
         for(let j = 0; j < SIZE; j++)
         {
             let g_cell = g_row.insertCell(j);
+            g_cell.id = i.toString() + j.toString();
             if(board[i][j] != 0)
             {
                 g_cell.innerHTML = Sol[i][j];
@@ -66,11 +70,13 @@ $(function(){
     }
     let p_cell = p_row.insertCell(-1);
     p_cell.innerHTML = "undo icon";
+    p_cell.id = "undo";
 });
 
-// Assign palette selected value to selectedVal when clicked, for user to place number on game board
+// Assign palette selected value to selectedVal when clicked; for user to place number on game board
 let selectedVal = "";
 let selectedID = "";
+let stack = [];
 $(function(){
     $("#Palette_Board td").on("click", function(){
         if(this.id[0] == "p")
@@ -93,22 +99,47 @@ $(function(){
                 document.getElementById(selectedID).classList.remove("selected");
                 selectedVal = "";
                 selectedID = "";
-            } 
+            }
+        }
+        else if(this.id == "undo")
+        {
+            if(stack.length > 0)
+            {
+                // assign popped element to undoVal
+                let undoVal = stack.pop();
+                // assign undoVal value[0] to the cell by ID
+                let undoCell = document.getElementById(undoVal[1])
+                undoCell.innerHTML = undoVal[0];
+                undoCell.classList.remove(document.getElementById(undoVal[1]).className);
+            }
         }
         
     })
 });
 
+// place value to empty cell and push previous value to stack
 $(function(){
     $("#Game_Board td").on("click", function(){
 
-        if(this.innerHTML == "")
+        if(this.innerHTML == "" && selectedVal != "")
         {
+            stack.push([this.innerHTML,this.id]);
             this.innerHTML = selectedVal;
+            checkInput(this);
         }
     });
 });
-  
+
+// apply different background color to the correct/incorrect input
+function checkInput(item)
+{
+    if(item.innerHTML == Sol[item.id[0]][item.id[1]])
+    {
+        item.classList.add("correct");
+        stack.pop();
+    }
+    else item.classList.add("error");
+}
 
 
 
